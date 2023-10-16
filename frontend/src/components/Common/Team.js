8aa8ca38c529da91f0364c1daea4f2fe68cdab62
ainidr25 <ainiidirhmwtii25@gmail.com>
2023-10-16 10:31:43 +0700

    LandingPage V2

--- frontend/src/components/Common/Team.js
+++ frontend/src/components/Common/Team.js
@@ -13,18 +13,18 @@ const Team = () => {
         <div className="pt-100 pb-70">
             <div className="container">
                 <div className="section-title">
-                    <h2>Skilled Team</h2>
+                    <h2>Moderate-2 Team</h2>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
                 </div>
 
                 <div className="row justify-content-center">
-                    <div className="col-lg-3 col-sm-6">
+                    <div className="col-lg-2 col-sm-4">
                         <div className="team-card text-center">
                             <img src="../images/team/circle-team1.jpg" alt="Image" className="rounded-circle" />
 
                             <div className="team-caption">
-                                <h3>James White</h3>
-                                <p>CEO & Founder</p>
+                                <h3>Aini Diah</h3>
+                                <p>211524033</p>
                                 <ul>
                                     <li>
                                         <a href="linkedin.com" target="_blank">
@@ -37,18 +37,8 @@ const Team = () => {
                                         </a>
                                     </li>
                                     <li>
-                                        <a href="https://www.facebook.com/" target="_blank">
-                                            <i className="icofont-facebook"></i>
-                                        </a>
-                                    </li>
-                                    <li>
                                         <a href="https://www.instagram.com/" target="_blank">
                                             <i className="icofont-instagram"></i>
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="https://dribbble.com/" target="_blank">
-                                            <i className="icofont-dribbble"></i>
                                         </a>
                                     </li>
                                 </ul>
@@ -56,13 +46,13 @@ const Team = () => {
                         </div>
                     </div>
 
-                    <div className="col-lg-3 col-sm-6">
+                    <div className="col-lg-2 col-sm-4">
                         <div className="team-card text-center">
-                            <img src="../images/team/circle-team2.jpg" alt="Image" className="rounded-circle" />
+                            <img src="../images/team/circle-team1.jpg" alt="Image" className="rounded-circle" />
 
                             <div className="team-caption">
-                                <h3>Johnson Lee</h3>
-                                <p>Backend Specialist</p>
+                                <h3>Ghessa Theniana</h3>
+                                <p>2115240</p>
                                 <ul>
                                     <li>
                                         <a href="linkedin.com" target="_blank">
@@ -75,18 +65,8 @@ const Team = () => {
                                         </a>
                                     </li>
                                     <li>
-                                        <a href="https://www.facebook.com/" target="_blank">
-                                            <i className="icofont-facebook"></i>
-                                        </a>
-                                    </li>
-                                    <li>
                                         <a href="https://www.instagram.com/" target="_blank">
                                             <i className="icofont-instagram"></i>
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="https://dribbble.com/" target="_blank">
-                                            <i className="icofont-dribbble"></i>
                                         </a>
                                     </li>
                                 </ul>
@@ -94,13 +74,13 @@ const Team = () => {
                         </div>
                     </div>
 
-                    <div className="col-lg-3 col-sm-6">
+                    <div className="col-lg-2 col-sm-4">
                         <div className="team-card text-center">
-                            <img src="../images/team/circle-team3.jpg" alt="Image" className="rounded-circle" />
+                            <img src="../images/team/circle-team1.jpg" alt="Image" className="rounded-circle" />
 
                             <div className="team-caption">
-                                <h3>Williams Smith</h3>
-                                <p>Front-end Specialist</p>
+                                <h3>Helsa Alika</h3>
+                                <p>2115240</p>
                                 <ul>
                                     <li>
                                         <a href="linkedin.com" target="_blank">
@@ -113,18 +93,8 @@ const Team = () => {
                                         </a>
                                     </li>
                                     <li>
-                                        <a href="https://www.facebook.com/" target="_blank">
-                                            <i className="icofont-facebook"></i>
-                                        </a>
-                                    </li>
-                                    <li>
                                         <a href="https://www.instagram.com/" target="_blank">
                                             <i className="icofont-instagram"></i>
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="https://dribbble.com/" target="_blank">
-                                            <i className="icofont-dribbble"></i>
                                         </a>
                                     </li>
                                 </ul>
@@ -132,13 +102,13 @@ const Team = () => {
                         </div>
                     </div>
 
-                    <div className="col-lg-3 col-sm-6">
+                    <div className="col-lg-2 col-sm-4">
                         <div className="team-card text-center">
-                            <img src="../images/team/circle-team4.jpg" alt="Image" className="rounded-circle" />
+                            <img src="../images/team/circle-team1.jpg" alt="Image" className="rounded-circle" />
 
                             <div className="team-caption">
-                                <h3>David Miller</h3>
-                                <p>Reactjs Specialist</p>
+                                <h3>Muhammad Daffa</h3>
+                                <p>2115240</p>
                                 <ul>
                                     <li>
                                         <a href="linkedin.com" target="_blank">
@@ -151,18 +121,36 @@ const Team = () => {
                                         </a>
                                     </li>
                                     <li>
-                                        <a href="https://www.facebook.com/" target="_blank">
-                                            <i className="icofont-facebook"></i>
+                                        <a href="https://www.instagram.com/" target="_blank">
+                                            <i className="icofont-instagram"></i>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="col-lg-2 col-sm-4">
+                        <div className="team-card text-center">
+                            <img src="../images/team/circle-team1.jpg" alt="Image" className="rounded-circle" />
+
+                            <div className="team-caption">
+                                <h3>Muhammad Rafi</h3>
+                                <p>2115240</p>
+                                <ul>
+                                    <li>
+                                        <a href="linkedin.com" target="_blank">
+                                            <i className="icofont-linkedin"></i>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="https://twitter.com/" target="_blank">
+                                            <i className="icofont-twitter"></i>
                                         </a>
                                     </li>
                                     <li>
                                         <a href="https://www.instagram.com/" target="_blank">
                                             <i className="icofont-instagram"></i>
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="https://dribbble.com/" target="_blank">
-                                            <i className="icofont-dribbble"></i>
                                         </a>
                                     </li>
                                 </ul>
