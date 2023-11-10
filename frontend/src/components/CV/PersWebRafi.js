1b39c93e628b7664d431c4c43d4ce76082fbeb4b
MuhammadRafiFarhan <mrafif.sman.daci@gmail.com>
2023-11-10 07:16:36 +0700

    Personal Web

--- frontend/src/components/CV/PersWebRafi.js
+++ frontend/src/components/CV/PersWebRafi.js
@@ -31,41 +31,76 @@ const PersWebRafi = () => {
           <h1 className="text-4xl font-bold mb-8 text-center">
             Personal Web Rafi
           </h1>
+          
+          <div className="bg-white rounded-lg shadow-lg p-6 mb-6">
+            <div>
+              <img
+                src={`${baseUrl}${data.data_diri.foto}`}
+                alt=""
+                className="mask mask-squircle mx-auto mb-4 w-80 h-auto"
+              />
+            </div>
+          </div>
+
           <div className="bg-white rounded-lg shadow-lg p-6 mb-6">
             <h2 className="text-xl font-semibold mb-4 text-center">
               Personal Information
             </h2>
 
-            <div>
-              <img
-                src={`${baseUrl}${data.data_diri.foto}`}
-                alt=""
-                className="mask mask-squircle mx-auto mb-4 w-40 h-auto"
-              />
-            </div>
-
             <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mb-6">
               <div>
-                <p>Nama: {data.data_diri.nama}</p>
-                <p>Tempat Lahir: {data.data_diri.tempat_lahir}</p>
-                <p>Tanggal Lahir: {data.data_diri.tanggal_lahir}</p>
-                <p>Usia: {data.data_diri.usia}</p>
-                <p>Tinggi Badan: {data.data_diri.tinggi_badan}</p>
-                <p>Berat Badan: {data.data_diri.berat_badan}</p>
+                <p>
+                  <th>Nama:</th> {data.data_diri.nama}
+                </p>
+                <p>
+                  <th>Tempat Lahir:</th> {data.data_diri.tempat_lahir}
+                </p>
+                <p>
+                  <th>Tanggal Lahir:</th> {data.data_diri.tanggal_lahir}
+                </p>
+                <p>
+                  <th>Usia:</th> {data.data_diri.usia}
+                </p>
+                <p>
+                  <th>Tinggi Badan:</th> {data.data_diri.tinggi_badan}
+                </p>
+                <p>
+                  <th>Berat Badan:</th> {data.data_diri.berat_badan}
+                </p>
               </div>
               <div>
-                <p>Alamat: {data.data_diri.alamat}</p>
-                <p>Agama: {data.data_diri.agama}</p>
-                <p>Jenis Kelamin: {data.data_diri.jenis_kelamin}</p>
-                <p>Telp: {data.data_diri.telp}</p>
-                <p>Email: {data.data_diri.email}</p>
+                <p>
+                  <th>Alamat:</th> {data.data_diri.alamat}
+                </p>
+                <p>
+                  <th>Agama:</th> {data.data_diri.agama}
+                </p>
+                <p>
+                  <th>Jenis Kelamin:</th> {data.data_diri.jenis_kelamin}
+                </p>
+                <p>
+                  <th>Telp:</th> {data.data_diri.telp}
+                </p>
+                <p>
+                  <th>Email:</th> {data.data_diri.email}
+                </p>
               </div>
             </div>
-            <p>Status: {data.data_diri.status}</p>
-            <p>Instagram: {data.data_diri.instagram}</p>
-            <p>Linkedin: {data.data_diri.linkedin}</p>
-            <p>Github: {data.data_diri.github}</p>
-            <p>Deskripsi: {data.data_diri.deskripsi}</p>
+            <p>
+              <th>Status:</th> {data.data_diri.status}
+            </p>
+            <p>
+              <th>Instagram:</th> {data.data_diri.instagram}
+            </p>
+            <p>
+              <th>Linkedin:</th> {data.data_diri.linkedin}
+            </p>
+            <p>
+              <th>Github:</th> {data.data_diri.github}
+            </p>
+            <p>
+              <th>Deskripsi:</th> {data.data_diri.deskripsi}
+            </p>
           </div>
 
           <div className="bg-white rounded-lg shadow-lg p-6 mb-6">
