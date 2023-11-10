fb27b52adf3c1a00831467967074c9e63efc0339
MuhammadRafiFarhan <mrafif.sman.dac@gmail.com>
2023-11-11 01:09:47 +0700

    Personal Web + PDF Fix Multiple Page Usage

--- frontend/src/components/dashboard.js
+++ frontend/src/components/dashboard.js
@@ -1,12 +1,12 @@
+// dashboard.js
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import jwt_decode from "jwt-decode";
 import Sidebar from "./Navigation/sidebar";
 import { FaBars } from "react-icons/fa";
-import jsPDF from "jspdf";
-import 'jspdf-autotable';
 import Navbar2 from './Navigation/navbar2';
 import { useNavigate } from "react-router-dom";
+import generatePDF from "./pdfGenerator";
 
 const Dashboard = () => {
   const [nama, setNama] = useState("");
@@ -78,103 +78,8 @@ const Dashboard = () => {
     }
   };
 
-  const generatePDF = () => {
-    if (!cvData) {
-      alert("CV data is not available. Please wait for the data to load.");
-      return;
-    }
-  
-    const doc = new jsPDF();
-  
-    // Set document properties (optional)
-    doc.setProperties({
-      title: "My CV",
-      author: "Your Name",
-    });
-  
-    // Set the initial y-coordinate
-    let startY = 10;
-  
-    // Define a function to add data and table with a header
-    function addDataAndTable(title, data, header) {
-      if (startY + 20 > 280) {
-        doc.addPage();
-        startY = 10;
-      }
-      doc.text(title, 10, startY);
-      startY += 10;
-      doc.autoTable({
-        head: [header], // Header row
-        body: data,
-        startY,
-        theme: "striped",
-      });
-      startY = doc.autoTable.previous.finalY + 10;
-    }
-  
-    // Add personal data to the CV
-    const personalHeader = ["Attribute", "Value"];
-    const personalData = [
-      ["Name", cvData.personal.nama],
-      ["Date of Birth", cvData.personal.tanggal_lahir],
-      ["Place of Birth", cvData.personal.tempat_lahir],
-      ["Age", cvData.personal.usia],
-      ["Height", cvData.personal.tinggi_badan],
-      ["Weight", cvData.personal.berat_badan],
-      ["Address", cvData.personal.alamat],
-      ["Religion", cvData.personal.agama],
-      ["Gender", cvData.personal.jenis_kelamin],
-      ["Phone", cvData.personal.telp],
-      ["Email", cvData.personal.email],
-      ["Marital Status", cvData.personal.status],
-      ["Instagram", cvData.personal.instagram],
-      ["LinkedIn", cvData.personal.linkedin],
-      ["GitHub", cvData.personal.github],
-    ];
-    addDataAndTable("Personal Information", personalData, personalHeader);
-  
-    // Add education data to the CV
-    const educationHeader = ["Institution", "Major", "Start Year", "End Year"];
-    const educationData = cvData.education.map((edu) => [
-      edu.instansi_pendidikan,
-      edu.jurusan,
-      edu.tahun_mulai_ajaran,
-      edu.tahun_akhir_ajaran,
-    ]);
-    addDataAndTable("Education", educationData, educationHeader);
-  
-    // Add organization data to the CV
-    const organizationHeader = ["Organization", "Position", "Start Date", "End Date"];
-    const organizationData = cvData.organization.map((org) => [
-      org.nama_organisasi,
-      org.posisi,
-      org.tanggal_mulai_menjabat,
-      org.tanggal_akhir_menjabat,
-    ]);
-    addDataAndTable("Organizations", organizationData, organizationHeader);
-  
-    // Add skills data to the CV
-    const skillsHeader = ["Skill", "Capability"];
-    const skillsData = cvData.skills.map((skill) => [
-      skill.nama_skill,
-      skill.capability + "%",
-    ]);
-    addDataAndTable("Skills", skillsData, skillsHeader);
-  
-    // Add portfolio data to the CV
-    const portfolioHeader = ["Portfolio", "Description"];
-    const portfolioData = cvData.portfolio.map((portfolio) => [
-      portfolio.nama_portofolio,
-      portfolio.deskripsi_portofolio,
-    ]);
-    addDataAndTable("Portfolio", portfolioData, portfolioHeader);
-  
-    const pdfDataUri = doc.output("datauristring");
-  
-    const newTab = window.open();
-    newTab.document.write(
-      '<iframe width="100%" height="100%" src="' + pdfDataUri + '"></iframe'
-    );
+  const generatePDFHandler = () => {
+    generatePDF(cvData);
   };
 
   return (
@@ -205,7 +110,7 @@ const Dashboard = () => {
               </div>
               <div className="bg-white p-4 rounded-lg shadow-md w-1/4 mt-5">
                 <p className="text-lg">Generate CV to PDF</p>
-                <button onClick={generatePDF} className="btn btn-outline btn-success btn-sm mt-2">Click</button>
+                <button onClick={generatePDFHandler} className="btn btn-outline btn-success btn-sm mt-2">Click</button>
               </div>
             </div>
           ) : (
