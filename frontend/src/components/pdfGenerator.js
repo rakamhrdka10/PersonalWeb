5b1d4413e29640e6ded7c26dd2e4ad30bbf0a860
MuhammadRafiFarhan <mrafif.sman.dac@gmail.com>
2023-11-21 13:23:15 +0700

    Perbaikan CV PDF

--- frontend/src/components/pdfGenerator.js
+++ frontend/src/components/pdfGenerator.js
@@ -17,7 +17,25 @@ const generatePDF = (cvData) => {
   });
 
   // Set the initial y-coordinate
-  let startY = 10;
+  let startY = 20;
+
+  // Add separating line
+  doc.setLineWidth(0.5);
+  doc.line(14, startY, 196, startY);
+  doc.line(14, startY + 1, 196, startY + 1);
+  startY += 10;
+
+  // Add header with "Curriculum vitae" text
+  doc.setFontSize(18);
+  doc.setFont("bold");
+  doc.text("Curriculum vitae", 105, startY, { align: "center" });
+  startY += 5;
+
+  // Add separating line
+  doc.setLineWidth(0.5);
+  doc.line(14, startY, 196, startY);
+  doc.line(14, startY + 1, 196, startY + 1);
+  startY += 20;
 
   // Define a function to add data and table with a header
   function addDataAndTable(title, data, header) {
@@ -25,35 +43,54 @@ const generatePDF = (cvData) => {
       doc.addPage();
       startY = 10;
     }
+    doc.setTextColor(0, 0, 0); // Set text color to black
     doc.text(title, 10, startY);
-    startY += 10;
-    doc.autoTable({
-      head: [header], // Header row
+    startY += 5;
+
+    // Check if it's the personal data table
+    const isPersonalTable = title === "Personal Information";
+
+    const tableOptions = {
       body: data,
       startY,
-      theme: "striped",
-    });
-    startY = doc.autoTable.previous.finalY + 10;
+      theme: "striped", // You can remove this line if you don't want alternating row colors
+      styles: {
+        lineColor: isPersonalTable ? [255, 255, 255] : [0, 0, 0], // White color for personal data table
+        lineWidth: isPersonalTable ? 0 : 0.1, // No border width for personal data table
+        fillColor: [255, 255, 255], // Add this line to remove the background color
+        textColor: [0, 0, 0], // Set text color to black
+      },
+      columnWidth: "auto", // Set the width of columns automatically
+    };
+
+    // Check if it's the personal data table and customize styles
+    if (!isPersonalTable) {
+      tableOptions.head = [header]; // Header row
+    }
+
+    doc.autoTable(tableOptions);
+
+    startY = doc.autoTable.previous.finalY + (isPersonalTable ? 10 : 15);
   }
 
   // Add personal data to the CV
   const personalHeader = ["Attribute", "Value"];
   const personalData = [
-    ["Name", cvData.personal.nama],
-    ["Date of Birth", cvData.personal.tanggal_lahir],
-    ["Place of Birth", cvData.personal.tempat_lahir],
-    ["Age", cvData.personal.usia],
-    ["Height", cvData.personal.tinggi_badan],
-    ["Weight", cvData.personal.berat_badan],
-    ["Address", cvData.personal.alamat],
-    ["Religion", cvData.personal.agama],
-    ["Gender", cvData.personal.jenis_kelamin],
-    ["Phone", cvData.personal.telp],
-    ["Email", cvData.personal.email],
-    ["Marital Status", cvData.personal.status],
-    ["Instagram", cvData.personal.instagram],
-    ["LinkedIn", cvData.personal.linkedin],
-    ["GitHub", cvData.personal.github],
+    ["Name", ":", cvData.personal.nama],
+    ["Date of Birth", ":", cvData.personal.tanggal_lahir],
+    ["Place of Birth", ":", cvData.personal.tempat_lahir],
+    ["Age", ":", cvData.personal.usia],
+    ["Height", ":", cvData.personal.tinggi_badan],
+    ["Weight", ":", cvData.personal.berat_badan],
+    ["Address", ":", cvData.personal.alamat],
+    ["Religion", ":", cvData.personal.agama],
+    ["Gender", ":", cvData.personal.jenis_kelamin],
+    ["Phone", ":", cvData.personal.telp],
+    ["Email", ":", cvData.personal.email],
+    ["Marital Status", ":", cvData.personal.status],
+    ["Instagram", ":", cvData.personal.instagram],
+    ["LinkedIn", ":", cvData.personal.linkedin],
+    ["GitHub", ":", cvData.personal.github],
   ];
   addDataAndTable("Personal Information", personalData, personalHeader);
 
@@ -68,7 +105,12 @@ const generatePDF = (cvData) => {
   addDataAndTable("Education", educationData, educationHeader);
 
   // Add organization data to the CV
-  const organizationHeader = ["Organization", "Position", "Start Date", "End Date"];
+  const organizationHeader = [
+    "Organization",
+    "Position",
+    "Start Date",
+    "End Date",
+  ];
   const organizationData = cvData.organization.map((org) => [
     org.nama_organisasi,
     org.posisi,
