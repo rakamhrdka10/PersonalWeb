48ddb3ca9f3e38d0eb726025b2566058cb965ff8
helsaalika <helsa.alika.tif421@polban.ac.id>
2023-12-03 08:20:59 +0700

    personal web helsa

--- frontend/src/components/CV/PersWebHelsa.js
+++ frontend/src/components/CV/PersWebHelsa.js
@@ -31,11 +31,16 @@ const PersWebHelsa = () => {
       {data && data.data_diri ? (
         <div>
           {/* Tampilan pertama */}
-          <div className="hero min-h-screen bg-base-200">
+          <div
+            className="hero min-h-screen"
+            style={{
+              background: "linear-gradient(to right, #94B9FF, #FFCDF9)",
+            }}
+          >
             <div className="hero-content flex-col lg:flex-row-reverse">
               <div className="text-center">
                 <h1 className="text-5xl font-bold">
-                  Halo, I am {data.data_diri.nama}
+                  Hello, I am {data.data_diri.nama}
                 </h1>
                 <p className="py-6">{data.data_diri.deskripsi}</p>
                 <button className="btn btn-primary">Download My CV</button>
@@ -108,9 +113,11 @@ const PersWebHelsa = () => {
               <div className="text-center">
                 <ul className="steps steps-vertical">
                   {data.data_diri.pendidikans.map((pendidikan, index) => (
-                    <li className="step" key={index}>
-                      <i>{pendidikan.tahun_mulai_ajaran}</i> <br></br>
-                      {pendidikan.instansi_pendidikan}
+                    <li className="step step-secondary" key={index}>
+                      <div className="my-10 text-left">
+                        <i>{pendidikan.tahun_mulai_ajaran}</i>
+                        <p>{pendidikan.instansi_pendidikan}</p>
+                      </div>
                     </li>
                   ))}
                 </ul>
@@ -122,37 +129,71 @@ const PersWebHelsa = () => {
                   Organizational Experience
                 </h1>
               </div>
-              <div className="">
-                {data.data_diri.organisasis.map((organisasi, index) => (
-                  <p>
-                    <b>
-                      <i>
-                        {organisasi.tanggal_mulai_menjabat} -
-                        {organisasi.tanggal_akhir_menjabat}
-                      </i>
-                    </b>
-                  </p>
-                ))}
+              <div className="text-center">
+                <ul className="steps steps-vertical">
+                  {data.data_diri.organisasis.map((organisasi, index) => (
+                    <li className="step step-accent" key={index}>
+                      <div className="my-10 text-left">
+                        <i>{`${organisasi.tanggal_mulai_menjabat} - ${organisasi.tanggal_akhir_menjabat}`}</i>{" "}
+                        <p>{`${organisasi.nama_organisasi} (${organisasi.posisi})`}</p>
+                      </div>
+                    </li>
+                  ))}
+                </ul>
               </div>
             </div>
           </div>
           <div>
-            <div className="mt-5 min-h-screen">
+            <div className="mt-5">
               <div className="text-center">
                 <h1 className="text-3xl font-bold">Check Out My Skills !</h1>
               </div>
-              {data.data_diri.skills.map((skill, index) => (
-                <div className="flex-col">
-                  <div className="text-center p-3">
-                    <div
-                      className="radial-progress mr-5"
-                      style={{ "--value": skill.capability, "--size": "9rem" }}
-                      role="progressbar"
-                    >
-                      {skill.capability}%
+              <div className="flex w-full justify-evenly my-10 flex-col md:flex-row">
+                {data.data_diri.skills.map((skill, index) => (
+                  <>
+                    <div className="text-center p-3">
+                      <div
+                        className="radial-progress"
+                        style={{
+                          "--value": skill.capability,
+                          "--size": "9rem",
+                          "--thickness": "2px",
+                        }}
+                        role="progressbar"
+                      >
+                        {skill.capability}%
+                      </div>
+                      <div className="mt-3">
+                        <p>{skill.nama_skill}</p>
+                      </div>
                     </div>
-                    <div className="">
-                      <p>{skill.nama_skill}</p>
+                  </>
+                ))}
+              </div>
+            </div>
+          </div>
+          <div className="mt-5">
+            <div className="text-center">
+              <h1 className="text-3xl font-bold">
+                Take a look at my Portofolios
+              </h1>
+            </div>
+            <div className="flex w-full justify-evenly my-10 flex-col md:flex-row p-2">
+              {data.data_diri.portofolios.map((portofolio, index) => (
+                <div>
+                  <div className="card card-compact w-96 bg-base-100 shadow-xl text-center mb-5">
+                    <figure className="w-96 h-64 overflow-hidden">
+                      <img
+                        src={`${baseUrl}${portofolio.file_portofolio}`}
+                        alt={`portofolio ${index + 1}`}
+                        className="object-cover w-full h-full"
+                      />
+                    </figure>
+                    <div className="card-body text-left">
+                      <h5 className="font-bold">
+                        {portofolio.nama_portofolio}
+                      </h5>
+                      <p>{portofolio.deskripsi_portofolio}</p>
                     </div>
                   </div>
                 </div>
